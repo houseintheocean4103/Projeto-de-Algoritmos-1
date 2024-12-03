f7f533e0a2e7d33050d14c3ac1c75affbf1ae757
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-03 11:09:31 -0300

    Update Q1.cpp

--- Lista2/Q1.cpp
+++ Lista2/Q1.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+  int valor, canhao, polvora, espada;
+  cout<<"Digite o valor inicial ";
+  cin>>valor;
+  
+  canhao = valor/10000;
+  int resto = valor%10000;
+  
+  polvora = resto/2000;
+  resto = resto%2000;
+  espada = resto/1500;
+
+  cout<<canhao<<endl<<polvora<<endl<<espada;
+
+  return 0;
+}
