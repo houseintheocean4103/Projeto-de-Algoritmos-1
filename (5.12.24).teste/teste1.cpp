36b95acf6c1ce807a394636f4aecd4db9b0e08c0
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-07 17:09:52 -0300

    Update teste1.cpp

--- (5.12.24).teste/teste1.cpp
+++ (5.12.24).teste/teste1.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 using namespace std;
-int main () {  //sem bool
+int main () {
 	int vetA[10], vetB[4];
-	int i,j,n,cont;
+	int i,j,n,cont; //sem bool
 	
-	cout<<"Escreva os numeros"<<endl;
+	cout<<"Escreva os numeros"<<endl;  
 	for (i=0; i<10; i++)
-		cin>>vetA[i];
+		cin>>vetA[i]; 	
 
-	cout<<"Escreva as posicoes"<<endl;	
+	cout<<"Escreva as posicoes"<<endl;
 	for (i=0; i<4; i++)
 		cin>>vetB[i]; //correção:variável de  controle mudada para 'i' para se adequar ao 'for'
 	
-	for (i=0; i<4;i++) {
+	for (i=0; i<4;i++) { //correção:fim(i<10) está fora do escopo de 'vetB' pois este tem tamanho '[4]'
 		n = vetA[ vetB[i] ];
 
-		if (n == 2 or n == 1)
+		if (n == 1)  //falta o caso do 'n = 1' e 'n = 2'
 			cout<<n<<" nao e primo"<<endl;
+
+        if (n == 2)
+			cout<<n<<" e primo"<<endl;    
 
 		if (n > 2){	
 
-			cont = 0;
-			for ( j = 2; j < n; j++) {
+			cont = 0; //'cont' inicializada
+			for ( j = 2; j < n; j++) { //intervalo deve ser 'inicio(j=2)' e 'fim(j<n)' para que '1' e o próprio número 'n' não sejam divisores
 				if (n%j==0)
 					cont++;
 			}
-			if (cont == 0){
+			if (cont == 0){ //se 'cont' não mudar 'n' é primo
 				cout<<n<<" e primo"<<endl;
 	
-			} else {
+			} else { //se 'cont' mudar 'n' não é primo
 				cout<<n<<" nao e primo"<<endl;
 			}
 		}	
