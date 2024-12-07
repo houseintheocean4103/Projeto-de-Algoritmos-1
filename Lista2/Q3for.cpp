aa44b1bc9c707762585a4b0724921574568e7a4e
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-07 12:33:38 -0300

    Add files via upload

---
+++ Lista2/Q3for.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int valor, i;
+
+    cout<<"Digite o número: ";
+    cin>>valor;
+
+    for(i = 1; i <= 10; i++){
+        cout << valor*i << endl;
+    }
+}
