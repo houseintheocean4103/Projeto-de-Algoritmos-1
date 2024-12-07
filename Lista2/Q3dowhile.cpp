1a8972717f2c0b54ed36cc1787c66a97789c5696
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-07 12:32:44 -0300

    Create Q3dowhile.cpp

---
+++ Lista2/Q3dowhile.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int valor, i = 1;
+
+    cout << "Digite o número: ";
+    cin >> valor;
+
+    do { //o código é executado ao menos uma vez, independentemente do resto do loop
+        cout << valor << " x " << i << " = " << valor * i << endl;
+        i++;  // Incrementa o valor de i
+    } while (i <= 10);  // Condição para continuar o loop
+
+    return 0;
+}
