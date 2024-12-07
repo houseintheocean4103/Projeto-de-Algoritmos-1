69e089e12d04d9c700ce9b6fc03dd2f5506bd9c6
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-07 19:31:16 -0300

    Create teste2.cpp

---
+++ (5.12.24).teste/teste2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main () {
+	int vetA[10], vetB[4];
+	int i,j,n,cont;
+	bool temPrimo=false;
+
+    cout<<"Digite os numeros"<<endl;
+	for (i=0; i<10; i++)
+		cin>>vetA[i];
+	
+    cout<<"Digite as posicoes"<<endl;
+    for (i=0; i<4; i++)
+		cin>>vetB[i];  //correção:variável de  controle mudada para 'i' para se adequar ao 'for'
+	
+    for (i=0; i<4;i++) { //correção:fim(i<10) está fora do escopo de 'vetB' pois este tem tamanho '[4]'
+		n = vetA[ vetB[i] ]; //falta o caso do 'n = 1' e 'n = 2'
+
+        if (n == 1 or n == 0)
+            cout<<n<<" nao e primo"<<endl;
+        
+        if (n == 2){
+            cout<<n<<" e primo"<<endl;
+            temPrimo = true;
+            }
+        if (n > 2){    
+        	cont = 0;
+			for ( j = 2; j < n; j++) { //intervalo deve ser 'inicio(j=2)' e 'fim(j<n)' para que '1' e o próprio número 'n' não sejam divisores
+				if (n%j==0)
+					cont++; //falta inicializar 'cont'
+			}
+			if (cont == 0) { //'cont' deve ser igual a '0' para que o número seja primo
+				cout<<n<<" e primo"<<endl;
+				temPrimo=true;
+			}
+		}	
+	}
+	if (not temPrimo)
+		cout<<-1<<"/nao ha primos";	
+	return 0;	
+}
