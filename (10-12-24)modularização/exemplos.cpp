1a51ebfb28a9ac91029d28a39c9903edeea61cc8
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-10 14:40:33 -0300

    Update exemplos.cpp

--- (10-12-24)modularização/exemplos.cpp
+++ (10-12-24)modularização/exemplos.cpp
@@ -1,1 +1,29 @@
 
+#include <iostream>
+using namespace std;
+
+int paraCima(float n){
+    return int(n) + 1;
+}
+int paraBaixo(float n){
+    return int(n);
+}
+int convencional(float n){
+    if(n - int(n) < 0.5)
+        return int(n);
+      //return paraBaixo(n);  
+    else
+        return int(n) + 1;
+      //return paraCima(n);  
+}
+
+int main(){
+    float nume;
+
+    cout<<"Digite um numero"<<endl;
+    cin>>nume;
+
+    cout<<"Arredondado para cima: "<<paraCima(nume)<<endl;
+    cout<<"Arredondado para baixo: "<<paraBaixo(nume)<<endl;
+    cout<<"Arredondado convencional: "<<convencional(nume)<<endl;
+}
