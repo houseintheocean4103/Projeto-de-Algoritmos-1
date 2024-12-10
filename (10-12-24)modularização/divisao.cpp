93cb192526d667660f5654084327f561a9cd19c8
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-10 14:42:18 -0300

    Update divisao.cpp

--- (10-12-24)modularização/divisao.cpp
+++ (10-12-24)modularização/divisao.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int divisao(int n1, int n2, int &resto){
+    resto = n1 % n2;
+    return n1/n2;
+}
+
+int main(){
+    int n1, n2, resto;
+    //vamos dividir n1 por n2
+    //
+}
