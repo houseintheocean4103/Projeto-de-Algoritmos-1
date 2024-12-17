9b9eee99d07b1f22a04dd6567fefa4e1607ac127
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-17 10:15:25 -0300

    Update Q1.cpp

--- (10_12_24)_modul_Lista4/Q1.cpp
+++ (10_12_24)_modul_Lista4/Q1.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int paraCima(float n){
-    return int(n) + 1;
-}
+    return int(n) + 1; //int(float n) reduz n ao último inteiro
+}                      //soma-se mais um para pular para o próximo
 int paraBaixo(float n){
     return int(n);
 }
 int convencional(float n){
-    if(n - int(n) < 0.5)
+    if(n - int(n) < 0.5)    //float(n) - int(n) extrai a parte decimal
         return int(n);
       //return paraBaixo(n);  
     else
