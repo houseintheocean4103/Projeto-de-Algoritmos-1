87b604282efc6ba6c0e75f9796f6ed878f4b6951
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-17 10:27:31 -0300

    Update Q5.cpp

--- (10_12_24)_modul_Lista4/Q5.cpp
+++ (10_12_24)_modul_Lista4/Q5.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int buscavet(int V[], int N, int A){
-    int i;
+    int i;       //vetores dentro do escopo de subprogramas
 
     for(i=0;i<N;i++){
         if(V[i] == A)
-            return i;
-    }
-    return -1;
+            return i; //o primeiro return alcançado será o único a ser executado
+    }                 //dessa forma, se o loop 'for' falhar, 'return -1' será executado
+    return -1;        //e se o loop suceder, 'return i' será executado
 }
 
 int main(){
@@ -27,7 +27,7 @@ int main(){
 
     cout<<"Digite que elemento quer busccar no vetor:"<<endl;
     cin>>a;
-
+                                                //nome do vet ao utilizar a função
     cout<<"A posicao do elemento e: "<<buscavet(vet, n, a);
 
     return 0;
