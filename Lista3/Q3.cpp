a5b2f338975eaa14f2a0cd7b5e3f9f584a618fc1
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2024-12-16 22:45:17 -0300

    Create Q3.cpp

---
+++ Lista3/Q3.cpp
@@ -0,0 +1,22 @@
+//tira a raiz quadrada de elementos de um vetor
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int vet[5], i;
+    float raiz[5];
+
+    cout<<"Digite 5 numeros"<<endl;
+    for(i = 0;i < 5;i++)
+        cin>>vet[i];
+
+    cout<<"As raizes quadradas sao: "<<endl;
+    for(i = 0;i < 5;i++){
+        raiz[i] = sqrt(vet[i]);
+        cout<<raiz[i]<<" "; 
+    } 
+    
+    return 0;
+}
