0325c8d65b1f07c6d517bdfddce70bb178baa7d1
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2025-02-18 22:46:36 -0300

    Update lista_dinâmica_encadeada_lista.hpp

--- (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_lista.hpp
+++ (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_lista.hpp
@@ -1,22 +1,25 @@
 struct no {
-      char  dado; 
-      no *prox; //eh um ponteiro
+    char  dado; 
+    no *prox; //eh um ponteiro
 };
 
 struct Lista {
-	no *inicio;
-	no *fim; 
-	
+  no *inicio;
+  no *fim; 
+  
 };
 
-void cria(Lista*);
-int tamanho(Lista*);
-void inserirInicio (Lista *, char );
-void inserirPosicao (Lista *, char, char);
-void imprimir(Lista *);
-void inserirFim(Lista *, char );
-bool estaVazia(Lista *);
-void remover(Lista *, char);
-int esta_na_lista(Lista*,char);
-//void finaliza(Lista*);
+void cria(Lista*); //em sala
+int tamanho(Lista*); //em sala
+bool estaVazia(Lista *); //em sala
+void imprimir(Lista *); //em sala
+int esta_na_lista(Lista*,char); //dever de casa
+void inserirInicio (Lista *, char ); //em sala
+void inserirFim(Lista *, char ); //em sala
+void inserirPosicao (Lista *, char, int); //dever de casa/feito
+void removerInicio(Lista *); //em sala/refeito
+void removerFim(Lista *); //em sala/refeito
+void removerPosicao(Lista *, int pos); //extra/feito
+void removerElemento(Lista *, char); //dever de casa/feito
 
+//void finaliza(Lista*);
