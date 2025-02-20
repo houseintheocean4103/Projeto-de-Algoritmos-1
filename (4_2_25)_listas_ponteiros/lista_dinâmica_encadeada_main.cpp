79ede7ea89c5d758ae4882d0aa70d7d768c3fd48
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2025-02-19 23:54:39 -0300

    Update lista_dinâmica_encadeada_main.cpp

--- (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_main.cpp
+++ (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_main.cpp
@@ -3,29 +3,35 @@
 using namespace std;
 
 int main (){
+
+    //bloco de declaração e criação
     Lista minha_lista;
     cria(&minha_lista);
 
+    //bloco de inserção
     inserirInicio(&minha_lista, 'A');
     inserirInicio(&minha_lista, 'B');
     inserirInicio(&minha_lista, 'C');
     inserirFim(&minha_lista, 'Z');
     inserirFim(&minha_lista, 'W');
     inserirPosicao(&minha_lista, 'H', 1);
+
+    //bloco de impressão
+    imprimir(&minha_lista);
+    cout<<tamanho(&minha_lista)<<endl;
+
+    //bloco de remoção
+    removerInicio(&minha_lista);
+    removerFim(&minha_lista);
+    removerPosicao(&minha_lista, 2);
+    removerElemento(&minha_lista, 'A');
+
     imprimir(&minha_lista);
     cout<<tamanho(&minha_lista)<<endl;
     
-    
+    //bloco lógico
+    cout<<esta_na_lista(&minha_lista, 'H')<<endl;    
     cout<<estaVazia(&minha_lista)<<endl;
-    //removerInicio(&minha_lista);
-    //removerFim(&minha_lista);
-    //removerPosicao(&minha_lista, 2);
-    //removerElemento(&minha_lista, 'A');
-    //imprimir(&minha_lista);
-    
-    //cout<<tamanho(&minha_lista)<<endl;
-
-    cout<<esta_na_lista(&minha_lista, 'H')<<endl;
 
     return 0;
 }
