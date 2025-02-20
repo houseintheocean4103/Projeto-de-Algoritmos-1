26d2e3e9627e49939efaf1452cdaf74df5678625
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2025-02-19 23:53:08 -0300

    Update lista_dinâmica_encadeada_lista.hpp

--- (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_lista.hpp
+++ (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_lista.hpp
@@ -1,19 +1,19 @@
-struct no {
-    char  dado; 
-    no *prox; //eh um ponteiro
+struct no { //struct para nós
+  char  dado; 
+  no *prox; //eh um ponteiro
 };
 
-struct Lista {
-  no *inicio;
-  no *fim; 
-  
+struct Lista { //struct para inicio e fim da lista
+no *inicio;
+no *fim; 
+
 };
 
 void cria(Lista*); //em sala
 int tamanho(Lista*); //em sala
 bool estaVazia(Lista *); //em sala
 void imprimir(Lista *); //em sala
-int esta_na_lista(Lista*,char); //dever de casa
+int esta_na_lista(Lista*,char); //dever de casa/feito
 void inserirInicio (Lista *, char ); //em sala
 void inserirFim(Lista *, char ); //em sala
 void inserirPosicao (Lista *, char, int); //dever de casa/feito
