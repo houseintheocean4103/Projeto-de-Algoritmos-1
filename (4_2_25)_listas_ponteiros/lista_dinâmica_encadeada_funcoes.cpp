098f2fbf04babc7f87fda7bd4977ebe725b10a85
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2025-02-19 23:52:11 -0300

    Update lista_dinâmica_encadeada_funcoes.cpp

--- (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_funcoes.cpp
+++ (4_2_25)_listas_ponteiros/lista_dinâmica_encadeada_funcoes.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 void cria (Lista *L){
-    L->inicio = NULL;
-    L->fim = NULL;
+    L->inicio = NULL; //ponteiros para marcar os elementos início e final da lista
+    L->fim = NULL;    //declarados
 }
 
 int tamanho(Lista *L){
-    no* aux;
+    no* aux;      //declara ponteiro auxiliar para manipular elementos
     int cont = 0;
     aux=L->inicio;
-    while (aux != NULL){
-        cont++;
-        aux = aux->prox;
+    while (aux != NULL){ //enquanto aux não chegar no último elemento(apontar para NULL)
+        cont++;          //incrementa 'cont'
+        aux = aux->prox; //atribui-se a 'aux' o valor apontado por ela
     }
-    return cont;
+    return cont; //ao final 'cont' deve equivaler ao número de elementos
 }
 
 void imprimir(Lista *L){
@@ -23,70 +23,72 @@ void imprimir(Lista *L){
     no *aux;
     aux=L->inicio;
     while (aux != NULL){
-        cout<<aux->dado<<endl;
+        cout<<aux->dado<<endl; //para cada iteração imprime o valor armazenado no campo 'dado' do elemento
         aux = aux->prox;
     }
 }
 
 bool estaVazia(Lista *L){
 
-    if(L->inicio==NULL)
-        return true; //1
+    if(L->inicio==NULL) //basta checar se 'inicio' está vazio
+        return true; //1 
     return false; //0
 
 }
 
 int esta_na_lista(Lista *L, char info){
 
-    int exist = 0;
+    int exist = 0; //valor permanecerá se o teste falhar, ou seja, nada foi encontrado 
     no *aux;
     aux=L->inicio;
 
     while(aux != NULL){        
-        if(aux->dado == info){
-            exist = 1;                   
+        if(aux->dado == info){ //se o valor for achado, altera 'exist' para 1
+            exist = 1;         
         }
         aux = aux->prox;
     }
-    return exist;
+    return exist; //retorna resposta relativa às saídas booleanas
 }   
 
 void inserirInicio(Lista *L, char info){
     //criar o nó
-    no *novo = new no;
-    novo->dado = info;
+    no *novo = new no; //aloca espaço para o novo elemento(objeto)
+    novo->dado = info; //recebe no campo 'dado' o valor
 
     if (L->inicio == NULL){ //se lista vazia
-        novo->prox = NULL;
-        L->inicio = novo;
-        L->fim = novo;
+        novo->prox = NULL;  //por ser o único também é o final, então deve apontar para NULL
+        L->inicio = novo;   //'inicio' aponta para 'novo'
+        L->fim = novo;      //'fim' aponta para 'novo'
     } else {
-        novo->prox=L->inicio;
-        L->inicio=novo;
+        novo->prox=L->inicio; //novo passa a apontar para o antigo elemento de 'inicio'
+        L->inicio=novo;       //e o marcador'inicio' passa apontar para o 'novo'
     }
 }
 
 void inserirFim(Lista *L, char info){
 
-    no *novo = new no;
+    no *novo = new no; //cria movo nó
     novo->dado = info;
 
-    L->fim->prox=novo;
-    novo->prox=NULL;
+    novo->prox=NULL; //o novo nó deve apontar para NULL
+    L->fim->prox=novo; //o nó apontado por 'fim' passa a apontar para 'novo'
+    L->fim=novo; //'fim' passa apontar para novo 
+
 }
 
 void inserirPosicao(Lista *L, char info, int pos){
 
-    no *novo = new no;
+    no *novo = new no; //cria nó
     novo->dado=info;
 
-    if(pos == 0){
+    if(pos == 0){ //insere no inicio chamando a função
 
         inserirInicio(L, info);
 
     } else if(pos == tamanho(L) - 1){
 
-        inserirFim(L, info);
+        inserirFim(L, info); //insere no fim chamando a outra função
 
     } else {
 
@@ -95,25 +97,35 @@ void inserirPosicao(Lista *L, char info, int pos){
 
         aux=L->inicio;
 
-        while(cont != pos){
+        while(cont != pos){ //encontra o nó daquela posição
             aux=aux->prox;
             cont++;
         }
-        novo->prox=aux->prox;
-        aux->prox=novo;
+        novo->prox=aux->prox; //'novo' passa a apontar para onde o antigo nó apontava
+        aux->prox=novo; //o antigo nó passa a apontar para 'novo'
     }
 }
 
 void removerInicio(Lista *L){
 
-    L->inicio = L->inicio->prox;
+    L->inicio = L->inicio->prox; //'inicio' passa a apontar para o endereço que o nó apontado em 'inicio' aponta
 
 }
 
 void removerFim(Lista *L){
+    
+    no *aux, *aux_ant; //declara dois auxiliares
+    aux = L->inicio;
+    int cont = 0;
 
-    L->fim->prox=NULL;
+    while(cont != tamanho(L) - 1){ //loop até a última posição para evitar NULL
+        aux_ant = aux;   //guarda o valor da última iteração(penúltimo elemento)
+        aux = aux->prox; //muda de nó( para no último elemento)
+        cont++;
+    }
 
+    aux_ant->prox=NULL; //penúltimo passa a apontar para NULL
+    L->fim=aux_ant;     //'fim' passa a apontar para o penúltimo, tornando-o o último
 }
 
 void removerElemento(Lista *L, char info){
@@ -121,13 +133,13 @@ void removerElemento(Lista *L, char info){
     no *aux = L->inicio;
     no *aux_ant;
     
-    while(aux->dado != info){
-        aux_ant = aux;
-        aux = aux->prox;
+    while(aux->dado != info){ //compara os campos 'dado' do nó
+        aux_ant = aux;   //guarda o valor da última iteração(penúltimo elemento)
+        aux = aux->prox; //muda de nó(para no elemento solicitado)
     }
 
-    aux_ant->prox=aux->prox;
-    aux->prox=NULL;
+    aux_ant->prox=aux->prox; //'aux_ant' passa a apontar para o endereço que o nó solicitado apontava
+    aux->prox=NULL;          //o nó solicitado passa a apontar para NULL
 
 }
 
@@ -138,11 +150,22 @@ void removerPosicao(Lista *L, int pos){
 
     aux=L->inicio;
 
-    while(cont != pos){
-        aux_ant = aux;
-        aux=aux->prox;
-        cont++;
+    if(pos == 0){
+
+        removerInicio(L); //aplica a função
+
+    } else if(pos == tamanho(L) - 1){
+
+        removerFim(L); //aplica a função
+
+    } else {
+
+        while(cont != pos){
+            aux_ant = aux; //guarda o valor da última iteração(penúltimo elemento)
+            aux=aux->prox; //muda de nó(para no elemento da posição solicitada)
+            cont++;
+        }
+        aux_ant->prox=aux->prox; //'aux_ant' passa a apontar para o endereço que o nó da posição solicitada apontava
+        aux->prox=NULL; //o nó da posição solicitada passa a apontar para NULL
     }
-    aux_ant->prox=aux->prox;
-    aux->prox=NULL;
 }
