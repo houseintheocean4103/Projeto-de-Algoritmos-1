4edc3eeca6328894aec62036a8de567dc0af5274
houseintheocean4103 <gabriel.silva.6149@gmail.com>
2025-03-21 17:24:21 -0300

    Update pilha_4.cpp

--- execícios_estrutura_dados/pilha_4.cpp
+++ execícios_estrutura_dados/pilha_4.cpp
@@ -13,9 +13,51 @@ void empilhar(Pilha *minhapilha, char dado){
     if(minhapilha->topo == NULL){
         novo->prox = NULL;
     } else {
-        minhapilha->topo->prox = novo;
+        novo->prox = minhapilha->topo;
     }
     minhapilha->topo = novo;
 }
 
-void vogais(Pilha *minhapilha) 
+bool vogais(char letra){
+    
+    string vog = "aeiouAEIOU";
+
+    for(int i = 0;i < 10;i++)
+        if(letra == vog[i]){
+            return true;
+        }
+    return false;
+} 
+
+void imprimir(Pilha *minhapilha){
+
+    no *aux = minhapilha->topo;
+    int i = 0;
+
+    while(aux != NULL){
+        if(vogais(aux->valor)){
+            cout<<i<<endl;
+        }
+        i++;
+        aux = aux->prox;
+    }
+}
+
+int main(){
+
+    Pilha teste;
+    string palavra = "guardanapo";
+
+    cria_pilha(&teste);
+
+    for(int i = palavra.size() - 1;i >= 0;i--){
+        empilhar(&teste, palavra[i]);
+    }
+
+    imprimir(&teste);
+
+    
+
+}
+
+
